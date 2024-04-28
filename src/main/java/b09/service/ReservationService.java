6720cca93ed6cfd3adf2cdddef8d4cc718b911c7
twkwon0417 <kwontony123@konkuk.ac.kr>
2024-04-28 11:52:52 +0900

    feat: by Kim

--- src/main/java/b09/service/ReservationService.java
+++ src/main/java/b09/service/ReservationService.java
@@ -2,27 +2,31 @@ package b09.service;
 
 import b09.model.Member;
 import b09.model.Reservation;
+import b09.model.reservation.ReservedDate;
 import b09.model.reservation.RoomNumber;
 import b09.repository.MemberRepository;
 import b09.repository.ReservationRepository;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReservationService {
     private final ReservationRepository reservationRepository;
     private final MemberRepository memberRepository;
 
+    //생성자 메서드
     public ReservationService(ReservationRepository reservationRepository, MemberRepository memberRepository) {
         this.reservationRepository = reservationRepository;
         this.memberRepository = memberRepository;
     }
 
-
+    //한 멤버가 갖고 있는 예약 리스트 반환
     public List<Reservation> getMembersReservation(Member member) {
         List<Integer> reservations = member.getReservations();
         return reservations.stream().map(x -> reservationRepository.getReservationById(Long.valueOf(x))).toList();
     }
-
+    //새로운 예약이 들어왔을때
     public void registerReservation(Reservation reservation) {
         reservationRepository.registerReservation(reservation);
         Member memberToBeEdited = memberRepository.getMemberById(reservation.getMemberId());
@@ -32,15 +36,15 @@ public class ReservationService {
                 memberToBeEdited.getReservations());
         memberRepository.editMember(memberRepository.getMemberById(reservation.getMemberId()), newMember);
     }
-
+    //예약 삭제
     public void deleteReservation(Reservation reservation) {
         Member member = memberRepository.getMemberById(reservation.getMemberId());  // 멤버가 가지고 있는 예약들의 리스트를 수정
         member.getReservations().remove(Math.toIntExact(reservation.getId()));
         memberRepository.editMember(
                 member, new Member(
                         member.getPhoneNumber(),
-                        member.getTotalMoneySpent(),
-                        member.getReservations()
+                        member.getTotalMoneySpent(), //total money를 그대로 해도 되는지?
+                        member.getReservations()  //이것도
                 )
         );
 
@@ -49,7 +53,15 @@ public class ReservationService {
 
     public List<Reservation> findAllReservationOfDate(LocalDate date) {
         List<Reservation> all = reservationRepository.findAll();    // 인자의 date가 Reservation의 StartDate와 EndDate사이에 있는 친구들을 찾아 반환
-        return null;
+        List<Reservation> find = new ArrayList<>();
+        for(Reservation reservation : all){
+            LocalDate startDate = reservation.getReservedDate().getStartDate();
+            LocalDate endDate = reservation.getReservedDate().getEndDate();
+            if((startDate.isBefore(date) && endDate.isBefore(date)) || startDate.isEqual(date) || endDate.isEqual(date)){
+                find.add(reservation);
+            }
+        }
+        return find;
     }
 
     public void exchangeRoom(Reservation reservation, RoomNumber roomNumber, LocalDate todaysDate) throws Exception{
@@ -59,10 +71,46 @@ public class ReservationService {
         // ReservedDate에 public method로 LocalDate를 입력 받으면 LocalDate가 ReservedDate사이인지 알려주는 메서드 만들어서 활용하면 더 깔끔해질거 같은데, 편하신데로 하세욤
         // 절대 txt 파일이 수정 되어야 합니다.
         // 얘가 가장 복잡해 보이네요 화이팅~
+        //의미 규칙 : 이미 해당날짜의 호수에 예약이 있는 경우
+        //문법 규칙 : 객실 목록에 없는 문자열 입력, 해당 날짜의 호수에 예약이 있는 경우
+        for(Reservation findReserve : reservationRepository.findAll()){
+            if (findReserve.getRoomNumber().equals(roomNumber)){
+                LocalDate startDate = reservation.getReservedDate().getStartDate();
+                LocalDate endDate = reservation.getReservedDate().getEndDate();
+                if((startDate.isBefore(todaysDate) && endDate.isBefore(todaysDate)) || startDate.isEqual(todaysDate) || endDate.isEqual(todaysDate)){
+                    throw new Exception("이 객실은 이미 예약된 상태입니다. 다른 객실 호수를 입력해주세요.");
+                }else{
+                    LocalDate reservStart = reservation.getReservedDate().getStartDate();
+                    LocalDate reservEnd = reservation.getReservedDate().getEndDate();
+                    //해당 날짜의 객실이 빌경우
+                    if(todaysDate.isBefore(reservStart)){  //현재 날짜가 예약 전인지 체크
+                        registerReservation(new Reservation(reservation.getMemberId(), roomNumber, reservation.getReservedDate(), reservation.getNumberOfPeople(), reservation.getAdditionalProduct()));
+                        reservationRepository.delete(reservation);
+                    }else{ //이미 사용자가 이용중일 경우
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyMMdd");
+                        String str = reservStart.format(formatter);
+                        str += " ";
+                        str += todaysDate.minusDays(1).format(formatter);
+                        registerReservation(new Reservation(reservation.getMemberId(), reservation.getRoomNumber(), new ReservedDate(str), reservation.getNumberOfPeople(), reservation.getAdditionalProduct()));
+                        str = "";
+                        str += todaysDate.format(formatter);
+                        str += " ";
+                        str += reservEnd.format(formatter);
+                        registerReservation(new Reservation(reservation.getMemberId(), roomNumber, new ReservedDate(str), reservation.getNumberOfPeople(), reservation.getAdditionalProduct()));
+                        reservationRepository.delete(reservation);
+                    }
+                }
+            }
+        }
+
+
     }
 
     public void extendCheckoutDate(Reservation reservation, LocalDate todaysDate) throws Exception {
         // 의미규칙이 만족하지 않을 경우 예외를 던져야ㅑ 합니다. 에러의 예외 메세지 사용장에게 무엇떄문에 불가한지 출력이 되는데 쓰임으로 예쁘게(형식에 맞게)
         // 절대 txt 파일이 수정 되어야 합니다.
+
     }
+
+
 }
