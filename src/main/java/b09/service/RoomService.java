8c0ea38ec7267d13d80575ed0bb0cf8075322a0e
twkwon0417 <kwontony123@konkuk.ac.kr>
2024-04-29 05:29:38 +0900

    final

--- src/main/java/b09/service/RoomService.java
+++ src/main/java/b09/service/RoomService.java
@@ -2,7 +2,6 @@ package b09.service;
 
 import b09.model.Reservation;
 import b09.model.reservation.ReservedDate;
-import b09.model.reservation.RoomNumber;
 import b09.repository.ReservationRepository;
 
 import java.time.LocalDate;
@@ -46,37 +45,43 @@ public class RoomService{
                 int roomNumber = reservation.getRoomNumber().ofInt();
                 int floor = roomNumber / 100 - 1;
                 int roomNum = roomNumber % 100 - 1;
-                hotelrooms[floor][roomNum] = false;
+                if (roomType == 1 && floor >= 0 && floor <= 4) { // standard room
+                    hotelrooms[floor][roomNum] = false;
+                } else if (roomType == 2 && floor >= 5 && floor <= 7) { // premier room
+                    hotelrooms[floor][roomNum] = false;
+                } else if (roomType == 3 && floor == 8) { // suite room
+                    hotelrooms[floor][roomNum] = false;
+                }
             }
         }
 
         int startFloor = 0, endFloor = 0;
         switch (roomType) {
             case 1:
-                startFloor = 0; // standard 시작 층
-                endFloor = 4; // standard 종료 층
+                startFloor = 1; // standard 시작 층
+                endFloor = 5; // standard 종료 층
                 break;
             case 2:
-                startFloor = 5; // premier 시작 층
-                endFloor = 7; // premier 종료 층
+                startFloor = 6; // premier 시작 층
+                endFloor = 8; // premier 종료 층
                 break;
             case 3:
-                startFloor = 8; // suite 시작 층
-                endFloor = 8; // suite 종료 층
+                startFloor = 9; // suite 시작 층
+                endFloor = 9; // suite 종료 층
                 break;
         }
 
         for (int floor = startFloor; floor <= endFloor; floor++) {
             for (int roomNum = 0; roomNum < hotelrooms[floor].length; roomNum++) {
                 if (hotelrooms[floor][roomNum]) {
-                    availableRooms.add((floor+2) + "0" + (roomNum+1));
+                    String roomNumber = (floor + 1) * 100 + (roomNum + 1) + "";
+                    availableRooms.add(roomNumber);
                 }
             }
         }
 
         return availableRooms;
     }
-
     public void restrictRoom(Reservation reservation) {
         hotelrooms[reservation.getRoomNumber().ofInt() / 100][reservation.getRoomNumber().ofInt() % 11] = false;
     }
